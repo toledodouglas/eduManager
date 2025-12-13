0cdc7a05ac161aecdcf55928c54ee426f6b7723f
douglas <douglas.rtoledo2003@gmail.com>
2025-12-13 17:57:52 -0300

    feat: created repository class

--- EduManager.Domain/Interfaces/IRepository.cs
+++ EduManager.Domain/Interfaces/IRepository.cs
@@ -6,8 +6,8 @@ namespace EduManager.Domain.interfaces
 {
     public interface IRepository<T> where T : class
     {
-        Task<T> GetByIdAsync(int id);
         Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
         Task AddAsync(T entity);
         Task UpdateAsync(T entity);
         Task DeleteAsync(int id);
